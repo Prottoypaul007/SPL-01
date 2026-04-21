86a49596ceaec185d7c55f3497125c022bcb7e1d
Prottoy Paul <bsse1613@iit.du.ac.bd>
2026-04-21 14:49:05 +0600

    jar file

---
+++ Run_Project.bat.bat
@@ -0,0 +1,9 @@
+@echo off
+title Algorithm Optimization Suite
+echo Launching the Dashboard...
+
+:: This runs the JAR file just like the terminal
+java -jar OptimizationSuite.jar
+
+:: If Java isn't installed or crashes, pause keeps the window open so they can read the error
+pause
